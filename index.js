869714371484f6c45a37a7a35e9bf2ef61746f33
Sandeep Thapa <sandeep.thapa@outlook.com>
2019-06-17 11:23:15 +0300

    ROutes in server.js added for all the queries

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const db = require("/queries");
 const app = express();
 const port = 3000;
 
@@ -10,6 +11,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", (request, response) => {
   response.json({info: "An intro to using express, node and postgress."});
 });
+
+app.get("/users", db.getUsers);
+app.get("/users/:id", db.getUserById);
+app.post("/users", db.createUser);
+app.put("/users/:id", db.updateUser);
+app.delete("/users/:id", db.deleteUser);
 
 app.listen(port, () => {
   console.log("Server running succesfully");
